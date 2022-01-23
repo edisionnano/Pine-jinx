eb9549c9e88e0458be1c7695c810ac5f7c150261
Essasetic <34970796+Essasetic@users.noreply.github.com>
2022-01-23 09:50:21 +0000

    Remove hardcoding of the 1.1.4 version
    Makes it so it can be upgraded when a new version comes out.

--- pinejinx.sh
+++ pinejinx.sh
@@ -51,9 +51,9 @@ removealias() {
 }
 install() {
 	printf "Downloading $version...\n"
-	curl -L "https://github.com/Ryujinx/release-channel-master/releases/download/1.1.4/ryujinx-1.1.4-linux_x64.tar.gz" > ryujinx-1.1.4-linux_x64.tar.gz
-	tar -xf ryujinx-1.1.4-linux_x64.tar.gz
-	arch_dir=$(tar --exclude='*/*' -tf ryujinx-1.1.4-linux_x64.tar.gz)
+	curl -L "https://github.com/Ryujinx/release-channel-master/releases/download/${version}/ryujinx-${version}-linux_x64.tar.gz" > ryujinx-${version}-linux_x64.tar.gz
+	tar -xf ryujinx-${version}-linux_x64.tar.gz
+	arch_dir=$(tar --exclude='*/*' -tf ryujinx-${version}-linux_x64.tar.gz)
 	if [ -d "$arch_dir" ]; then
 		printf "Extraction successful!\n"
 		mkdir -p /home/${USER}/.local/share/Ryujinx
